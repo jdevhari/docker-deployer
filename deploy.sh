beffad3ef5d249d73ff10e855fe344e2d62ca88e
Hariprasath Manivannan <jdev.hari@gmail.com>
2017-05-05 15:04:26 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 docker stop $2
 docker rm $2
+
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
+
 docker pull $1/$2
 docker run --name $2 -d -p $3:$4 $1/$2
